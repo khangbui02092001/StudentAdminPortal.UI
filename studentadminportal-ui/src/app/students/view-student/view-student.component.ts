ff4e3e99c2e105e35f22c1b8295dcb8ad1b3ee03
khangbui <khangbuionlinegmail.com>
2022-09-09 22:18:26 +0700

    Done Section 6

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
@@ -9,76 +9,85 @@ import { StudentService } from '../student.service';
 @Component({
   selector: 'app-view-student',
   templateUrl: './view-student.component.html',
-  styleUrls: ['./view-student.component.css']
+  styleUrls: ['./view-student.component.css'],
 })
 export class ViewStudentComponent implements OnInit {
   studentId: string | null | undefined;
   student: Student = {
-    id:'',
-    firstName:'',
-    lastName:'',
-    dateOfBirth:'',
-    email:'',
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    email: '',
     mobile: 0,
     genderId: '',
-    profileImageUrl:'',
-    gender:{
-      id:'',
-      description:'',
+    profileImageUrl: '',
+    gender: {
+      id: '',
+      description: '',
     },
-    address:{
-      id:'',
-      physicalAddress:'',
-      postalAddress:''
-    }
-  }
+    address: {
+      id: '',
+      physicalAddress: '',
+      postalAddress: '',
+    },
+  };
 
   genderList: Gender[] = [];
 
-
-
-  constructor(private readonly studentService: StudentService,
+  constructor(
+    private readonly studentService: StudentService,
     private readonly route: ActivatedRoute,
     private readonly genderService: GenderService,
-    private snackbar: MatSnackBar) { }
+    private snackbar: MatSnackBar,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      (params)=>{
-        this.studentId = params.get('id');
+    this.route.paramMap.subscribe((params) => {
+      this.studentId = params.get('id');
 
-        if(this.studentId){
-          this.studentService.getStudent(this.studentId)
-          .subscribe(
-            (successResponse) => {
-              this.student = successResponse;
-            }
-          );
-          this.genderService.getGenderList()
-          .subscribe(
-            (successResponse) =>{
-              this.genderList = successResponse;
-            }
-          );
-        }
+      if (this.studentId) {
+        this.studentService
+          .getStudent(this.studentId)
+          .subscribe((successResponse) => {
+            this.student = successResponse;
+          });
+        this.genderService.getGenderList().subscribe((successResponse) => {
+          this.genderList = successResponse;
+        });
       }
-    );
+    });
   }
 
   onUpdate(): void {
-    this.studentService.updateStudent(this.student.id, this.student )
-    .subscribe(
-      (successResponse) =>{
+    this.studentService.updateStudent(this.student.id, this.student).subscribe(
+      (successResponse) => {
         // showw
-        this.snackbar.open('Student updated successfully', undefined,{
-          duration: 2000
+        this.snackbar.open('Student updated successfully', undefined, {
+          duration: 2000,
         });
       },
-      (errorResponse)=>{
-
-      }
-
+      (errorResponse) => {}
     );
   }
 
+  onDelete(): void {
+    this.studentService.deleteStudent(this.student.id).subscribe(
+      (successResponse) => {
+        this.snackbar.open('Student deleted Successfully', undefined,{
+          duration: 2000
+        });
+
+        setTimeout(()=>{
+          this.router.navigateByUrl('students')
+        }, 2000);
+
+
+      },
+      (errorResponse) => {
+        //log
+      }
+    );
+  }
 }
